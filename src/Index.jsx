2672190c0990176dba99e3017253bb7a3351ead4
Angel <avillanueva-2021436@kinal.edu.gt>
2023-05-01 22:51:17 -0600

    Se actualizo el index con rutas

--- src/Index.jsx
+++ src/Index.jsx
@@ -4,64 +4,69 @@ import { RouterProvider, createBrowserRouter } from 'react-router-dom'
 import { NotFound } from './pages/NotFound';
 import { HomePage } from './pages/HomePage';
 import { LoginPage } from './pages/LoginPage';
-import { DashboardPage } from './pages/DashboardPage/DashboardPage';
+import { DashboardPage } from './pages/DashboardPage/DashboardPage'
+import { ServiciosPage } from './pages/ServiciosPage/ServiciosPage'
+import { TableServicios } from './pages/ServiciosPage/TableServicios'
+import { AddServicios } from './pages/ServiciosPage/AddServicios'
+import { UpdateServicios } from './pages/ServiciosPage/UpdateServicios'
 
 export const AuthContext = createContext();
 
 export const Index = () => {
-    const [loggedIn, setLoggedIn] = useState(false)
-    const [dataUser, setDataUser] = useState({
-      name: '',
-      username: '',
-      role: ''
-    })
-    useEffect(()=> {
-      let token = localStorage.getItem('token')
-      if(token) setLoggedIn(true)
-    }, [])
+  const [loggedIn, setLoggedIn] = useState(true)
+  const [dataUser, setDataUser] = useState({
+    name: '',
+    username: '',
+    role: ''
+  })
+  useEffect(() => {
+    let token = localStorage.getItem('token')
+    if (token) setLoggedIn(true)
+  }, [])
 
-    const routes = createBrowserRouter([
+  const routes = createBrowserRouter([
+    {
+      path: '/',
+      element: <App />,
+      errorElement: <NotFound />,
+      children: [
         {
           path: '/',
-          element: <App/>,
-          errorElement: <NotFound/>,
+          element: <HomePage />
+        },
+        {
+          path: '/login',
+          element: <LoginPage></LoginPage>
+        },
+        {
+          path: '/dashboard',
+              element: loggedIn ? <DashboardPage/> : <LoginPage/>,
+              children: []
+        },
+        {
+          path: '/servicios',
+          element: loggedIn ? <ServiciosPage /> : <LoginPage/>,
           children: [
             {
-              path: '/',
-              element: <HomePage/>
+              path: '',
+              element: <TableServicios />
             },
             {
-              path: '/login',
-              element: <LoginPage></LoginPage>
+              path: 'add',
+              element: <AddServicios />
             },
             {
-              path: '/dashboard',
-              element: loggedIn ? <DashboardPage/> : <LoginPage/>,
-              children: [
-                /*{
-                  path: '',
-                  element: </>
-                },
-                {
-                  path: '',
-                  element: </>
-                },
-                {
-                  path: '',
-                  element: </>
-                },
-                {
-                  path: '',
-                  element: </>
-                }*/
-              ]
+              path: 'Update/:id',
+              element: <UpdateServicios />
             },
           ]
         }
-      ])
+      ]
+    }
+  ])
   return (
-    <AuthContext.Provider value={{loggedIn, setLoggedIn, dataUser, setDataUser}}>
-      <RouterProvider router={routes}/>
+    <AuthContext.Provider value={{ loggedIn, setLoggedIn, dataUser, setDataUser }}>
+      <RouterProvider router={routes} />
     </AuthContext.Provider>
   )
 }
